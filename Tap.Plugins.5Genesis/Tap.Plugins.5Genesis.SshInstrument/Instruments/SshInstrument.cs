9f278f9364a80c0fd75f91a238b213ee909310d2
Bruno <bgarcia@lcc.uma.es>
2019-02-27 14:22:50 +0100

    Implement connection, basic Run Command step

--- Tap.Plugins.5Genesis/Tap.Plugins.5Genesis.SshInstrument/Instruments/SshInstrument.cs
+++ Tap.Plugins.5Genesis/Tap.Plugins.5Genesis.SshInstrument/Instruments/SshInstrument.cs
@@ -2,18 +2,15 @@
 // Copyright:   Copyright 2019-2020 Universidad de Málaga (University of Málaga), Spain
 //
 // This file is part of the 5GENESIS project. The 5GENESIS project is funded by the European Union’s
-// Horizon 2020 research and innovation programme, grant agreement No <>.
+// Horizon 2020 research and innovation programme, grant agreement No 815178.
 //
 // This file cannot be modified or redistributed. This header cannot be removed.
 
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.ComponentModel;
 using Keysight.Tap;
-
+using Renci.SshNet;
 
 namespace Tap.Plugins._5Genesis.SshInstrument.Instruments
 {
@@ -21,23 +18,80 @@ namespace Tap.Plugins._5Genesis.SshInstrument.Instruments
     [ShortName("SSH")]
     public class SshInstrument : Instrument
     {
-        #region Settings
-        
-        #endregion
+        private SshClient ssh = null;
 
+        #region Settings
+
+        [Display("Host", Group: "Connection", Order: 0.1)]
+        public string Host { get; set; }
+
+        [Display("Port", Group: "Connection", Order: 0.2)]
+        public int Port { get; set; }
+
+        [Display("User", Group: "Credentials", Order: 1.1)]
+        public string User { get; set; }
+
+        [Display("Password/Passphrase", Group: "Credentials", Order: 1.2)]
+        [PasswordPropertyText]
+        public Enabled<string> Password { get; set; }
+
+        [Display("Private Key", Group: "Credentials", Order: 1.3)]
+        [FilePath(FilePathAttribute.BehaviorChoice.Open)]
+        public Enabled<string> Key { get; set; }
+       
+        #endregion
+        
         public SshInstrument()
         {
-            
+            Host = User = string.Empty;
+            Port = 22;
+            Password = new Enabled<string>() { IsEnabled = true, Value = string.Empty };
+            Key = new Enabled<string>() { IsEnabled = false, Value = string.Empty };
+
+            Rules.Add(() => !string.IsNullOrWhiteSpace(User), "Please select a host", "Host");
+            Rules.Add(() => Port > 0, "Please select a valid port number", "Port");
+            Rules.Add(() => Password.IsEnabled || Key.IsEnabled, "Please select an authentication method", "Password");
+            Rules.Add(() => !Password.IsEnabled || !string.IsNullOrWhiteSpace(Password.Value), "Please select a password value", "Password");
+            Rules.Add(() => !Key.IsEnabled || !string.IsNullOrWhiteSpace(Key.Value), "Please select a key file", "Key");
+            Rules.Add(() => !string.IsNullOrWhiteSpace(User), "Please select an user name", "User");
         }
 
         public override void Open()
         {
             base.Open();
+            AuthenticationMethod authentication;
+
+            if (Key.IsEnabled)
+            {
+                PrivateKeyFile privateKey = Password.IsEnabled ? new PrivateKeyFile(Key.Value, Password.Value) : new PrivateKeyFile(Key.Value);
+                authentication = new PrivateKeyAuthenticationMethod(User, new PrivateKeyFile[] { privateKey });
+            }
+            else
+            {
+                authentication = new PasswordAuthenticationMethod(User, Password.Value);
+            }
+
+            ConnectionInfo connectionInfo = new ConnectionInfo(Host, Port, User, authentication);
+
+            ssh = new SshClient(connectionInfo) { KeepAliveInterval = new TimeSpan(0, 0, 5) };
+            ssh.Connect();
         }
 
         public override void Close()
         {
+            ssh.Disconnect();
             base.Close();
+        }
+
+        public string Run(string command)
+        {
+            if (ssh != null && ssh.IsConnected)
+            {
+                SshCommand c = ssh.CreateCommand(command);
+                string result = c.Execute();
+                return result;
+            }
+            return "Not connected";
         }
     }
 }
---
+++ Tap.Plugins.5Genesis/Tap.Plugins.5Genesis.SshInstrument/Steps/RunSshCommand.cs
@@ -0,0 +1,37 @@
+﻿// Author:      Bruno Garcia Garcia <bgarcia@lcc.uma.es>
+// Copyright:   Copyright 2019-2020 Universidad de Málaga (University of Málaga), Spain
+//
+// This file is part of the 5GENESIS project. The 5GENESIS project is funded by the European Union’s
+// Horizon 2020 research and innovation programme, grant agreement No 815178.
+//
+// This file cannot be modified or redistributed. This header cannot be removed.
+
+using Keysight.Tap;
+
+namespace Tap.Plugins._5Genesis.SshInstrument.Steps
+{
+    [Display("Run SSH Command", Group: "5Genesis", Description: "Send an ")]
+    public class RunSshCommand : TestStep
+    {
+        #region Settings
+        
+        [Display("Instrument", Group: "Instrument", Order: 1.0)]
+        public Instruments.SshInstrument Instrument { get; set; }
+
+        [Display("Command", Group: "Command", Order: 2.0)]
+        public string Command { get; set; }
+
+        #endregion
+
+        public RunSshCommand() 
+        {
+            Command = "uname -a";
+        }
+
+        public override void Run()
+        {
+            string res = Instrument.Run(Command);
+            Log.Info(res);
+        }
+    }
+}
