b7f234b922e58e5e91884ad042f15715260e039e
Bruno <bgarcia@lcc.uma.es>
2019-08-30 10:13:37 +0200

    Add year 1 demo project, step for parsing Nemergent KPI files

---
+++ Tap.Plugins.5Genesis/Tap.Plugins.5Genesis.Y1Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tap.Plugins.5Genesis.Y1Demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tap.Plugins.5Genesis.Y1Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c38fb927-8bcf-4ea9-8770-745cf62ec3fc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// If you are using GIT and git tags, this project is set up to automatically update 
+// these values when building in release mode.
+// To add a tag use 'git tag -a v1.0 -m "version 1.0"' from the command line.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
